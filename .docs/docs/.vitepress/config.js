21252f17668d50d91928237bac44cebc28d0a655
ryun <ryan@letsgolearn.com>
2023-01-22 12:31:45 -0800

    fix docs
    Signed-off-by: ryun <ryan@letsgolearn.com>

--- .docs/docs/.vitepress/config.js
+++ .docs/docs/.vitepress/config.js
@@ -4,36 +4,14 @@
 module.exports = {
     title: 'Inertia Vue Table',
     base: '/inertia-vue-table/',
-    outDir: '../docs/',
+    outDir: '../../docs/',
     themeConfig: {
-
-        // nav: nav(),
         sidebar: {
             '/guide/': sidebarGuide(),
-        },
-        // algolia: {
-        //     appId: 'W5CY2S9LMZ',
-        //     apiKey: 'c73409be45109b0c282f41b517909a34',
-        //     indexName: 'inertia-vue-table'
-        // },
-    },
+        }
+    }
 
 };
-
-// function nav() {
-//     return [
-//         {
-//             text: 'Docs',
-//             activeMatch: `^/(guide|style-guide|cookbook|examples)/`,
-//             items: [
-//                 {text: 'Guide', link: '/guide/introduction'},
-//                 {text: 'Tutorial', link: '/tutorial/'},
-//                 {text: 'Examples', link: '/examples/'},
-//                 {text: 'Quick Start', link: '/guide/quick-start'},
-//             ],
-//         },
-//     ];
-// }
 
 function sidebarGuide() {
     return [
